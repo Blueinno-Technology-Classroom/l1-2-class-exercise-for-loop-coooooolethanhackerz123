038b9f7ce376659b6b4823490332c921d8c97cad
cooooooolethanhackerz22113 <wangethanhk@gmail.com>
2024-01-13 11:43:53 +0800

    easy

--- main.py
+++ main.py
@@ -4,33 +4,41 @@ Q1:
 '''
 
 # TODO: Write your code here
-
+for i in range(0, 10001):
+    print(f'i becomes{i}')
 ##################################################
 '''
 Q2:
 '''
 
 # TODO: Write your code here
-
+sum = 0
+for i in range(0, 1000001):
+    sum += i
 ##################################################
 '''
 Q3:
 '''
 
 # TODO: Write your code here
-
+for i in range (2,13,2):
+    print(i)
 ##################################################
 '''
 Q4:
 '''
 
 # TODO: Write your code here
-
+sum = 97
+for i in range (97,0,-3):
+    print(i)
 ##################################################
 '''
 Q5:
 '''
 
+for i in range (1, 11):
+    print(i * i)
 # TODO: Write your code here
 
 ##################################################
@@ -39,5 +47,35 @@ Q6:
 '''
 
 # TODO: Write your code here
+for i in range (5):
+    print('*' * 5)
+##################################################
 
+'''
+Q7:
+'''
+
+# TODO: Write your code here
+for i in range (1,6):
+    print('*' * i)
+##################################################
+
+
+'''
+Q8:
+'''
+
+# TODO: Write your code here
+for i in range (6):
+    print("*" * (6-i))
+##################################################
+    
+
+    '''
+Q9:
+'''
+
+# TODO: Write your code here
+for i in range (1,6):
+    print(f"{i}" * i)
 ##################################################
